c147b80308d87b0ea985f9d8438a1e90c1ca7a5e
SombreroElGringo <florent.pailhes@hotmail.fr>
2018-08-22 00:24:43 +0200

    LINE Pay's URLs

---
+++ lib/types.ts
@@ -0,0 +1,34 @@
+export type Query = 
+  | QueryPayments
+  | QueryPaymentsAuthorizations
+  | QueryPaymentsPreApprovedPayCheck;
+
+export type QueryPayments = {
+  /**
+   * Transaction id to inquire.
+   */
+  transactionId?: string;
+  /**
+   * Order id to inquire.
+   */
+  orderId?: string;
+};
+
+export type QueryPaymentsAuthorizations = {
+  /**
+   * Transaction id to inquire.
+   */
+  transactionId?: string;
+  /**
+   * Order id to inquire.
+   */
+  orderId?: string;
+};
+
+export type QueryPaymentsPreApprovedPayCheck = {
+  /**
+   * Credit card authorization
+   * Set true to execute authorization payment in minimum amount by registered credit card.
+   */
+  creditCardAuth?: boolean;
+};
