683428abf861aa5431abe12ae5a54b7c724a8b4e
SombreroElGringo <florent.pailhes@hotmail.fr>
2018-08-25 11:54:41 +0200

    Excpetions

---
+++ lib/exceptions.ts
@@ -0,0 +1,42 @@
+export class LinePayError extends Error {
+  constructor(
+    message: string,
+    public code: string,
+    private originalError: Error,
+  ) {
+    super(message);
+  }
+}
+
+export class JSONParseError extends Error {
+  constructor(message: string, public raw: any) {
+    super(message);
+  }
+}
+
+export class RequestError extends Error {
+  constructor(
+    message: string,
+    public code: string,
+    private originalError: Error,
+  ) {
+    super(message);
+  }
+}
+
+export class ReadError extends Error {
+  constructor(private originalError: Error) {
+    super(originalError.message);
+  }
+}
+
+export class HTTPError extends Error {
+  constructor(
+    message: string,
+    public statusCode: number,
+    public statusMessage: string,
+    private originalError: Error,
+  ) {
+    super(message);
+  }
+}
